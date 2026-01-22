944a1430e20730d5b09df153c8503cd10c9fa2ea
Dvronichenko Kirill <gglade999@gmail.com>
2026-01-23 00:24:01 +0300

    #0 - Правки конфигов и путей

--- app/server/api/index.ts
+++ app/server/api/index.ts
@@ -4,7 +4,7 @@
  */
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-import app from '../dist/app.js'
+import app from '../src/app'
 
 export default async (req: VercelRequest, res: VercelResponse) => {
 	return app(req, res)
